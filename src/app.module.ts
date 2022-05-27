dbc7ea22206a07c2b3a0b65c2cb8f4eecf330b9e
denismiron <53906630+denismiron@users.noreply.github.com>
2022-05-27 18:08:51 +0300

    message

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { ConfigModule } from "@nestjs/config";
 import { Review } from "./reviews/reviews.model";
-import {Interface} from "./interface/interface.model";
-import {News} from "./news/news.model";
+import { Interface } from "./interface/interface.model";
+import { News } from "./news/news.model";
 import { Company } from "./companies/companies.model";
 import { Categories } from "./categories/categories.model";
 import { Dish } from "./dishes/diches.model";
@@ -13,30 +13,34 @@ import { InterfaceModule } from "./interface/interface.module";
 import { DishesModule } from "./dishes/dishes.module";
 import { CompaniesModule } from "./companies/companies.module";
 import { CategoriesModule } from "./categories/categories.module";
-import { FilesModule } from './files/files.module';
+import { FilesModule } from "./files/files.module";
 import { ServeStaticModule } from "@nestjs/serve-static";
-import * as path from 'path';
+import * as path from "path";
 
 @Module({
-  controllers:[],
-  providers:[],
-  imports:[
+  controllers: [],
+  providers: [],
+  imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`
     }),
     ServeStaticModule.forRoot({
-      rootPath: path.resolve(__dirname, 'static'),
+      rootPath: path.resolve(__dirname, "static")
     }),
     SequelizeModule.forRoot({
-    dialect: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    models: [Review, News, Interface, Dish, Company, Categories],
-    autoLoadModels: true
-  }),
+      dialect: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      ssl: true,
+      dialectOptions: {
+        "ssl": { "require": true }
+      },
+      models: [Review, News, Interface, Dish, Company, Categories],
+      autoLoadModels: true
+    }),
     ReviewsModule,
     NewsModule,
     InterfaceModule,
@@ -46,6 +50,6 @@ import * as path from 'path';
     FilesModule
   ]
 })
-export class AppModule{
+export class AppModule {
 
 }
