8fb3abc154aa33e7306b7eee05f08d55bd1d71e3
denismiron <53906630+denismiron@users.noreply.github.com>
2022-05-30 19:41:54 +0300

    message

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,12 +3,14 @@ import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 import { SequelizeModule } from "@nestjs/sequelize";
 import { Categories } from  "./categories.model"
+import { FilesModule } from "../files/files.module";
 
 @Module({
   controllers: [CategoriesController],
   providers: [CategoriesService],
   imports: [
-    SequelizeModule.forFeature([Categories])
+    SequelizeModule.forFeature([Categories]),
+    FilesModule
   ]
 })
 export class CategoriesModule {}
--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -3,12 +3,14 @@ import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
 import { SequelizeModule } from "@nestjs/sequelize";
 import { News } from  "./news.model"
+import { FilesModule } from "../files/files.module";
 
 @Module({
   controllers: [NewsController],
   providers: [NewsService],
   imports: [
-    SequelizeModule.forFeature([News])
+    SequelizeModule.forFeature([News]),
+    FilesModule
   ]
 })
 export class NewsModule {}
