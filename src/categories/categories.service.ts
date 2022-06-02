15b70c6c789fe120ace35ae2192712a07388c4fb
denismiron <53906630+denismiron@users.noreply.github.com>
2022-06-03 00:49:23 +0300

    message

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -13,9 +13,9 @@ export class CategoriesService {
   }
 
   async createCategory(dto: CreateCategoriesDto, imageRef: any) {
-    const fileName = await this.fileService.createFile(imageRef)
-    const uploadedName = await this.imagesService.uploadImage(fileName)
-    const category = await this.categoriesRepository.create({...dto, imageRef: uploadedName});
+    // const fileName = await this.fileService.createFile(imageRef)
+    const uploadedUrl = await this.imagesService.uploadImage(imageRef)
+    const category = await this.categoriesRepository.create({...dto, imageRef: uploadedUrl});
     return category;
   }
 
