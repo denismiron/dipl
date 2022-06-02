382e4eee6ed64ff56e877f25684fe46f58fe3c6c
denismiron <53906630+denismiron@users.noreply.github.com>
2022-06-03 00:03:51 +0300

    message

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from "@nestjs/common";
 const cloudinary = require('cloudinary').v2;
-
+cloudinary.config({
+  cloud_name: 'bistro-obed-bufet',
+  api_key: '629223456471373',
+  api_secret: 'Dl6-R9zsY116vEn12eVdmB9vkIs'
+});
 @Injectable()
 export class ImagesService {
   async uploadImage(imageRef: any){
