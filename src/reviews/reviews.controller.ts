0d2242f71b5494897d77125b52ec92fed383572c
denismiron <53906630+denismiron@users.noreply.github.com>
2022-06-08 12:43:50 +0300

    message

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,17 +1,21 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { CreateReviewDto } from "./dto/create-review.dto";
 import { ReviewsService } from "./reviews.service";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { Review } from "./reviews.model";
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private reviewsService: ReviewsService) {
   }
-
+  @ApiOperation({summary:"Создание отзыва"})
+  @ApiResponse({status:200, type: Review})
   @Post('/add')
   create(@Body() reviewDto: CreateReviewDto) {
     return this.reviewsService.createReview(reviewDto);
   }
-
+  @ApiOperation({summary:"Получение отзыва"})
+  @ApiResponse({status:200, type: [Review]})
   @Get()
   getAll(){
     return this.reviewsService.getAllReviews()
--- src/reviews/reviews.model.ts
+++ src/reviews/reviews.model.ts
@@ -1,4 +1,5 @@
 import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { ApiProperty } from "@nestjs/swagger";
 
 interface ReviewCreationAttrs{
   phone: string;
@@ -9,14 +10,19 @@ interface ReviewCreationAttrs{
 
 @Table({tableName: 'reviews'})
 export class Review extends Model<Review, ReviewCreationAttrs>{
+  @ApiProperty({example:'1', description:'идентификатор'})
   @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
   id: number;
+  @ApiProperty({example:'+22234564234', description:'номер телефона'})
   @Column({ type: DataType.STRING, unique: true, allowNull: false})
   phone: string;
+  @ApiProperty({example:'Всё было здорово!', description:'Сообщение пользователя'})
   @Column({ type: DataType.TEXT, allowNull: false})
   message: string;
+  @ApiProperty({example:'Спасибо за отзыв!', description:'Ответ администратора'})
   @Column({ type: DataType.TEXT, allowNull: true})
   adminMessage: string;
+  @ApiProperty({example:'Гоша', description:'Имя пользователя'})
   @Column({ type: DataType.STRING, allowNull: false})
   name: string;
 }
