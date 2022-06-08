0c181e95567fe2e9640db1442df91e85b5677e22
denismiron <53906630+denismiron@users.noreply.github.com>
2022-06-08 12:47:21 +0300

    message

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateReviewDto {
+  @ApiProperty({example:'+22234564234', description:'номер телефона'})
   readonly phone: string;
+  @ApiProperty({example:'Всё было здорово!', description:'Сообщение пользователя'})
   readonly message: string;
+  @ApiProperty({example:'Спасибо за отзыв!', description:'Ответ администратора'})
   readonly adminMessage: string;
+  @ApiProperty({example:'Гоша', description:'Имя пользователя'})
   readonly name: string;
   // прошивание в твердой обложке,
   // костя я тебя люблю рижик
