569953d22815c6e4aac0726f4542002713d588d7
denismiron <53906630+denismiron@users.noreply.github.com>
2022-06-12 23:46:28 +0300

    message

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,15 +2,17 @@ import {Body, Controller, Post} from '@nestjs/common';
 
 import {CreateUserDto} from "../users/dto/create-user.dto";
 import {AuthService} from "./auth.service";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
-@ApiTags("Сюда не лезем пока что")
+import { User } from "../users/users.model";
+
+@ApiTags("Авторизация")
 @Controller('auth')
 export class AuthController {
 
   constructor(private authService: AuthService) {
   }
-
+  @ApiOperation({summary:"Авторизация"})
   @Post('/login')
   login(@Body() userDto: CreateUserDto) {
     return this.authService.login(userDto)
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,14 @@
 
 import {IsEmail, IsString, Length} from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto {
-
+  @ApiProperty({example:'Email', description:'должен быть строкой и почтой'})
   @IsString({message: 'Должно быть строкой'})
   @IsEmail({}, {message: "Некорректный email"})
   readonly email: string;
+
+  @ApiProperty({example:'пароль', description:'должен быть правильным)'})
   @IsString({message: 'Должно быть строкой'})
   @Length(4, 16, {message: 'Не меньше 4 и не больше 16'})
   readonly password: string;
