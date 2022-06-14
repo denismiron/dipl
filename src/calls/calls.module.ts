3392217625c9fcd1327a33896e5d892e93fb8b2d
denismiron <53906630+denismiron@users.noreply.github.com>
2022-06-14 16:57:45 +0300

    message

--- src/calls/calls.module.ts
+++ src/calls/calls.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 
 import { SequelizeModule } from "@nestjs/sequelize";
-import { Categories } from "../categories/categories.model";
 import {CallsService} from "./calls.service";
 import { CallsController } from "./calls.controller";
+import { Call } from "./calls.model";
 
 @Module({
   controllers: [CallsController],
   providers: [CallsService],
   imports: [
-    SequelizeModule.forFeature([Categories]),
+    SequelizeModule.forFeature([Call]),
   ]
 })
 export class CallsModule {}
