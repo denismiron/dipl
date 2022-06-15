5ea4c56e824152a244cc77afc528a585749025ae
denismiron <53906630+denismiron@users.noreply.github.com>
2022-06-15 11:48:46 +0300

    .

--- src/clients/clients.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClientsController } from './clients.controller';
-
-@Module({
-  controllers: [ClientsController]
-})
-export class ClientsModule {}
