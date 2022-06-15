709f30da85d3cdf232803aafe8da66f95b1079f3
denismiron <53906630+denismiron@users.noreply.github.com>
2022-06-15 12:06:24 +0300

    Update images.service.ts

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,12 +1,7 @@
 import { Injectable } from "@nestjs/common";
 require('dotenv').config()
 
-const cloudinary = require('cloudinary').v2;
-cloudinary.config({
-  cloud_name: 'bistro-obed-bufet',
-  api_key: '629223456471373',
-  api_secret: 'Dl6-R9zsY116vEn12eVdmB9vkIs'
-});
+
 @Injectable()
 export class ImagesService {
   async uploadImage(imageRef: any){
