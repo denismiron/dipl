dce8380f0b6419d9ab233752cac39788956c1810
denismiron <53906630+denismiron@users.noreply.github.com>
2022-06-15 12:06:43 +0300

    Update images.service.ts

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 require('dotenv').config()
 
-
+const cloudinary = require('cloudinary').v2;
 @Injectable()
 export class ImagesService {
   async uploadImage(imageRef: any){
